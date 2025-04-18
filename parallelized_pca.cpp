2c72bf09f54f38e378bce0df87f3c18badec3fc4
Richard Xue <richard.xue@yale.edu>
2025-04-17 20:47:29 -0400

    added parlay

---
+++ parallelized_pca.cpp
@@ -0,0 +1,11 @@
+#include "parlaylib/include/parlay/primitives.h"
+#include "parlaylib/include/parlay/parallel.h"
+#include "parlaylib/include/parlay/sequence.h"
+#include "parlaylib/include/parlay/utilities.h"
+
+
+struct eigenpair {
+    double eigenvalue;
+    parlay::sequence<double> eigenvector;
+}
+
